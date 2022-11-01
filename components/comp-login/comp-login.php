4f39b76dc3d1c0acdf79c8cde0114cf1f8d535cc
Adam Borowski <adborowski@gmail.com>
2022-11-01 16:38:46 +0100

    Login comp fix

--- components/comp-login/comp-login.php
+++ components/comp-login/comp-login.php
@@ -4,7 +4,8 @@
 
         <?php include("components/comp-logotype/comp-logotype.php") ?>
 
-        <input class="txtInput" id="txtUsername" placeholder="Your Username" value="guest"></input>
+        <h1>For demo purposes, please use the default 'adam' account</h1>
+        <input class="txtInput" id="txtUsername" placeholder="Your Username" value="adam"></input>
         <input class="txtInput" type="password" id="txtPassword" placeholder="Your Password" value="123456789"></input>
                 
         <div id="btnLogin" class="btn btn-primary"><div>LOG IN</div></div>
